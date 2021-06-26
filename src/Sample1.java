b3512bffa5d2287eca811b1c49544a6db1a3c9be
vaibhav <codekul.vaibhav@gmail.com>
2021-06-26 18:10:34 +0530

    cammand in Sample class

--- src/Sample1.java
+++ src/Sample1.java
@@ -5,3 +5,25 @@ public class Sample1{
         System.out.println("In sample third line ");
     }
 }
+
+/**
+ *    git init
+ *    git remote add origin https://github.com/codekulvaibhav/GithubSaturday.git
+ *    git add .
+ *    git commit -m "Sample1 is added"
+ *    git push origin master
+ *    git pull origin master
+ *    git branch test
+ *    git status
+ *    git checkout test
+ *    git add .
+ *    git commit -m "sample 2 added"
+ *    git push origin test
+ *    git status
+ *    git chekout master
+ *    git checkout master
+ *    git merge test
+ *    git push origin master
+ *
+ *
+ */
