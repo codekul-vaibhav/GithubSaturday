2403821066c171b8c0cbc13a4ab55347245ac67f
vaibhav <codekul.vaibhav@gmail.com>
2021-06-26 18:00:00 +0530

    sample 2 added

--- src/Sample1.java
+++ src/Sample1.java
@@ -1,6 +1,7 @@
 public class Sample1{
     public static void main(String[] args) {
         System.out.println("In sample1 ");
-        System.out.println("In sample second line ");
+         System.out.println("In sample second line ");
+        System.out.println("In sample third line ");
     }
 }
